67ef172f781aef604b5225907d326f92cf944730
pwoch <43825825+pwoch@users.noreply.github.com>
2021-10-08 21:03:22 +0200

    Defence system location

---
+++ Assets/Scripts/BuildingController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingController : MonoBehaviour
+{
+    [SerializeField] private GameObject WallPrefab;
+    [SerializeField] private GameObject TowerPrefab;
+    [SerializeField] private GameObject GatePrefab;
+    public float Size;
+
+    private Vector3 target;
+    private Vector3 position;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            target = hit.point;
+            position.x = Mathf.Round(target.x / Size) * Size;
+            position.y = 0.5f;
+            position.z = Mathf.Round(target.z / Size) * Size;
+            if (Input.GetMouseButtonDown(1))
+            {
+                if (hit.collider.gameObject.layer == 9)
+                {
+                    Destroy(hit.collider.gameObject);
+                }
+                else
+                {
+                    var building = Instantiate(TowerPrefab, position, Quaternion.identity);
+                    building.layer = 9;
+                }
+                //wall.transform.rotation = Quaternion.Euler(new Vector3(-90, 0, 0));
+                //wall.transform.position = new Vector3(wall.transform.position.x, 5, wall.transform.position.z);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/DefenceController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenceController : MonoBehaviour
+{
+    [SerializeField] private GameObject archer;
+    [SerializeField] private GameObject army;
+    [SerializeField] private GameObject barrel;
+
+    private Color color;
+    private GameObject building;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+        if (Physics.Raycast(ray, out hit))
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (hit.collider.gameObject.layer == 9)
+                {
+                    if (building)
+                    {
+                        if (!building.Equals(hit.collider.gameObject))
+                        {
+                            building.GetComponentInChildren<Renderer>().material.color = color;
+                            building = hit.collider.gameObject;
+                            building.GetComponentInChildren<Renderer>().material.color = Color.green;
+                        }
+                        else
+                        {
+                            building.GetComponentInChildren<Renderer>().material.color = color;
+                            building = null;
+                        }
+                    }
+                    else
+                    {
+                        color = hit.collider.gameObject.GetComponentInChildren<Renderer>().material.color;
+                        building = hit.collider.gameObject;
+                        building.GetComponentInChildren<Renderer>().material.color = Color.green;
+                    }
+                }
+            }
+            if (building)
+            {
+                    if (Input.GetKeyDown(KeyCode.A))
+                    {
+                        Instantiate(army, building.transform);
+                    }
+                    if (Input.GetKeyDown(KeyCode.L))
+                    {
+                        Instantiate(archer, building.transform);
+                    }
+                    if (Input.GetKeyDown(KeyCode.B))
+                    {
+                        Instantiate(barrel, building.transform);
+                    }
+            }
+        }
+    }
+}
